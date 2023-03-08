2bcc98afdbcef0c1728ab2121f3fb43d036881e0
Chiara Camerota <camerota.chiara.c@gmail.com>
2023-03-08 18:45:05 +0100

    d

--- Clean_Data_script.R
+++ Clean_Data_script.R
@@ -3,8 +3,9 @@ library(readr)
 library(dplyr)
 
 library(tidyr)
-
-Data_Set= c('CTU-IoT-Malware-Capture-44-1','CTU-IoT-Malware-Capture-52-1','CTU-IoT-Malware-Capture-20-1','CTU-IoT-Malware-Capture-21-1','CTU-IoT-Malware-Capture-42-1','CTU-IoT-Malware-Capture-60-1','CTU-IoT-Malware-Capture-17-1','CTU-IoT-Malware-Capture-36-1','CTU-IoT-Malware-Capture-33-1','CTU-IoT-Malware-Capture-8-1','CTU-IoT-Malware-Capture-35-1','CTU-IoT-Malware-Capture-48-1','CTU-IoT-Malware-Capture-39-1','CTU-IoT-Malware-Capture-7-1','CTU-IoT-Malware-Capture-9-1','CTU-IoT-Malware-Capture-3-1','CTU-IoT-Malware-Capture-1-1', 'CTU-IoT-Malware-Capture-34-1','CTU-IoT-Malware-Capture-43-1', 'CTU-IoT-Malware-Capture-44-1','CTU-IoT-Malware-Capture-49-1')
+'CTU-IoT-Malware-Capture-17-1','CTU-IoT-Malware-Capture-33-1','CTU-IoT-Malware-Capture-39-1','CTU-IoT-Malware-Capture-43-1',
+Data_Set= c('CTU-IoT-Malware-Capture-44-1','CTU-IoT-Malware-Capture-52-1','CTU-IoT-Malware-Capture-20-1','CTU-IoT-Malware-Capture-21-1','CTU-IoT-Malware-Capture-42-1','CTU-IoT-Malware-Capture-60-1',
+            Data_Set= c('CTU-IoT-Malware-Capture-36-1','CTU-IoT-Malware-Capture-8-1','CTU-IoT-Malware-Capture-35-1','CTU-IoT-Malware-Capture-48-1','CTU-IoT-Malware-Capture-7-1','CTU-IoT-Malware-Capture-9-1','CTU-IoT-Malware-Capture-3-1','CTU-IoT-Malware-Capture-1-1', 'CTU-IoT-Malware-Capture-34-1', 'CTU-IoT-Malware-Capture-44-1','CTU-IoT-Malware-Capture-49-1')
 
 for (i in 1:length(Data_Set)) {
   CleanData(Data_Set[i])
@@ -13,31 +14,31 @@ for (i in 1:length(Data_Set)) {
 
 
 CleanData <-  function(name_file){
- 
-  
-  TEMP <- read_delim(paste(paste("C:/Users/chiar/PycharmProjects/PACKTACK/Data/",name_file, sep=""),".txt",sep=""), 
-                     delim = "\t", escape_backslash = TRUE, 
-                     escape_double = FALSE, col_names = FALSE,  col_types = cols(X9 = col_number(), X1= col_character() ), 
+
+
+  TEMP <- read_delim(paste(paste("/Users/chiara/PycharmProjects/PACKTACK/Data/",name_file, sep=""),".txt",sep=""),
+                     delim = "\t", escape_backslash = TRUE,
+                     escape_double = FALSE, col_names = FALSE,  col_types = cols(X9 = col_number(), X1= col_character() ),
                      comment = "#", trim_ws = TRUE, skip = 6)
-  
-  return(print(paste("\n Load il file: ",name_file, sep="") ))
-  
+
+  (print(paste(" Load il file: ",name_file, sep="") ))
+
   TEMP <- TEMP %>% separate(X21, c('css','lABEL', 'DETAIL.LABE'), sep="   ")
-  
-  return(print(paste("\n New variable done in: ",name_file, sep="") ))
-  
+
+  (print(paste(" New variable done in: ",name_file, sep="") ))
+
   colnames(TEMP) <- c("ts","uid" ,"id.orig_h","id.orig_p","id.resp_h","id.resp_p" , "proto" ,"service" ,
                       "duration","orig_bytes","resp_bytes","conn_state" ,"local_orig","local_resp","missed_bytes",
-                      "history","orig_pkts","orig_ip_bytes" ,"resp_pkts","resp_ip_bytes","tunnel_parents" ,"label", 
+                      "history","orig_pkts","orig_ip_bytes" ,"resp_pkts","resp_ip_bytes","tunnel_parents" ,"label",
                       "detailed.label")
-  
-  return(print(paste("\n Start to save: ",name_file, sep="") ))
-  
-  write.table(TEMP, file = paste(paste("C:/Users/chiar/PycharmProjects/PACKTACK/Data/CleanData/",name_file, sep=""),".txt",sep="")
+
+  TEMP <- na.omit(TEMP)
+  (print(paste(" Start to save: ",name_file, sep="") ))
+
+  write.table(TEMP, file = paste(paste("/Users/chiara/PycharmProjects/PACKTACK/CleanData/",name_file, sep=""),".txt",sep="")
               , sep = "\t",
               row.names = FALSE)
-  
-  return(print(paste("\n Saved: ",name_file, sep="") ))
+
+  return(print(paste("Saved: ",name_file, sep="") ))
 }
-  
- 
+
--- main.py
+++ main.py
@@ -1,16 +1,94 @@
-# This is a sample Python script.
+import functools
+import numpy as np
+import pandas as pd
+import sklearn
+from sklearn.datasets import load_iris
+import functools
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from scipy.stats import randint
 
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+dataset_name=['CTU-IoT-Malware-Capture-21-1','CTU-IoT-Malware-Capture-42-1', 'CTU-IoT-Malware-Capture-3-1']
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# LOAD DATA
+dati={}
+for data_name in dataset_name:
+    dati[data_name] = pd.read_csv(f"/Users/chiara/PycharmProjects/PACKTACK/CleanData/{data_name}.txt", sep="\t", header=0)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Standardizzation
+from sklearn import preprocessing
+import numpy as np
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+Ys_label={}
+Xs_fact, Xs_num, Xs_std={}, {},{}
+for dato in dati:
+    Ys_label[dato]= dati[dato].label.map({'Benign':0,'Malicious':1})
+    Xs_fact = dati[dato].drop(labels=["duration","orig_bytes","resp_bytes","local_orig","local_resp","missed_bytes","tunnel_parents" ,"label","detailed.label"], axis=1)
+    Xs_num[dato]= dati[dato].select_dtypes(include=np.number).drop(labels=['ts', 'id.orig_p', 'id.resp_p' ], axis=1)
+
+    Xs_std[dato] = preprocessing.scale(Xs_num[dato])
+
+# Split data
+X_train={}
+X_test={}
+y_train={}
+y_test={}
+
+for dato in dati:
+    X_train[dato], X_test[dato], y_train[dato], y_test[dato] = train_test_split(Xs_std[dato], Ys_label[dato], test_size=0.2)
+
+
+
+# FIND THE BEST PARAMETER
+
+best_rf={}
+for dato in dati:
+    param_dist = {'n_estimators': randint(50,500),
+                  'max_depth': randint(1,20)}
+
+    # Create a random forest classifier
+    rf = RandomForestClassifier()
+
+    # Use random search to find the best hyperparameters
+    rand_search = RandomizedSearchCV(rf,
+                                     param_distributions = param_dist,
+                                     n_iter=5,
+                                     cv=5)
+
+    # Fit the random search object to the data
+    rand_search.fit( X_train[dato], y_train[dato])
+    # Create a variable for the best model
+    best_rf[dato] = rand_search.best_estimator_
+
+    # Print the best hyperparameters
+    print(f'{dato} - Best hyperparameters:',  rand_search.best_params_)
+
+# Function for generate the RF_Classifier
+def generate_rf(X_train, y_train, X_test, y_test, best_param):
+    rf = best_param
+    rf.fit(X_train, y_train)
+    print ("rf score ", rf.score(X_test, y_test))
+    y_pred = rf.predict(X_test)
+    #accuracy = accuracy_score(y_test, y_pred)
+    return rf
+
+# Aggregation RF
+def combine_rfs(rf_a, rf_b):
+    rf_a.estimators_ = rf_a.estimators_  + rf_b.estimators_
+    rf_a.n_estimators = len(rf_a.estimators_)
+    return rf_a
+
+
+# TRAIN
+rfs=[]
+for dato in ['CTU-IoT-Malware-Capture-21-1','CTU-IoT-Malware-Capture-42-1','CTU-IoT-Malware-Capture-3-1' ]: #dati:
+    rfs.append( generate_rf(X_train[dato], y_train[dato], X_test[dato], y_test[dato], best_rf[dato]))
+rf_combined = functools.reduce(combine_rfs,rfs)
+# the combined model scores better than *most* of the component models
+X=np.concatenate([X_test['CTU-IoT-Malware-Capture-21-1'],X_test['CTU-IoT-Malware-Capture-42-1'],X_test['CTU-IoT-Malware-Capture-3-1']], axis=0)
+Y=np.concatenate([y_test['CTU-IoT-Malware-Capture-21-1'],y_test['CTU-IoT-Malware-Capture-42-1'], y_test['CTU-IoT-Malware-Capture-3-1']], axis=0)
+print ("rf combined score", rf_combined.score(X, Y))
